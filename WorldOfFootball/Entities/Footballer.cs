de8c54a6a391466415e7e95c2a2e9d27ca5a52f4
Dawid Jedruch <dawidszymonjedruch@gmail.com>
2022-04-21 16:51:12 +0200

    Add Footballer entity

---
+++ WorldOfFootball/Entities/Footballer.cs
@@ -0,0 +1,13 @@
+﻿namespace WorldOfFootball.Entities
+{
+    public class Footballer
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string Position { get; set; }
+        public string Nationality { get; set; }
+
+    }
+}
