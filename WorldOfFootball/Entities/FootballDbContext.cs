6b8e3e6642085d5700072c22df9b8f3580b74b33
Dawid Jedruch <dawidszymonjedruch@gmail.com>
2022-04-26 15:42:20 +0200

    create DbContext
    added entityframework.sqlserver and created DbContext

---
+++ WorldOfFootball/Entities/FootballDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WorldOfFootball.Entities
+{
+    public class FootballDbContext : DbContext
+    {
+        private string _connectionString = "Server=localhost\\SQLEXPRESS01;Database=master;Trusted_Connection=True;";
+        public DbSet<FootballClub> FootballClubs { get; set; }
+        public DbSet<Footballer> Footballers { get; set;}
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<FootballClub>()
+                .Property(x => x.Name)
+                .IsRequired()
+                .HasMaxLength(30);
+
+            modelBuilder.Entity<Footballer>()
+                .Property(x => x.LastName)
+                .IsRequired(); 
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_connectionString);
+        }
+    }
+}
