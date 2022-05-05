083e9d67fdfa1c9002d73981cb619edc7d124837
Dawid Jedruch <dawidszymonjedruch@gmail.com>
2022-05-05 16:47:31 +0200

    api controller

---
+++ WorldOfFootball/Controllers/FootballController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WorldOfFootball.Entities;
+
+namespace WorldOfFootball.Controllers
+{
+    [Route("api/footballClubs")]
+    public class FootballController : ControllerBase
+    {
+        private readonly FootballDbContext _dbcontext;
+
+        public FootballController(FootballDbContext dbContext)
+        {
+            _dbcontext = dbContext;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<FootballClub>> GetAll()
+        {
+            var footballClubs = _dbcontext
+                .FootballClubs
+                .ToList();
+
+            return Ok(footballClubs);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<FootballClub> Get([FromRoute] int id)
+        {
+            var footballClub = _dbcontext
+                .FootballClubs
+                .FirstOrDefault(r => r.Id == id);
+
+            if(footballClub is null)
+            {
+                return NotFound();
+            }
+
+            return Ok(footballClub);
+        }
+    }
+}
