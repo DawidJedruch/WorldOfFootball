29dd6648387134b18c8693de76b137b99b52bb6d
Dawid Jedruch <dawidszymonjedruch@gmail.com>
2022-08-12 12:13:05 +0200

    corrected user login

--- WorldOfFootball/Services/AccountService.cs
+++ WorldOfFootball/Services/AccountService.cs
@@ -67,14 +67,14 @@ namespace WorldOfFootball.Services
                 new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
                 new Claim(ClaimTypes.Name, $"{user.FirstName} {user.LastName}"),
                 new Claim(ClaimTypes.Role, $"{user.Role.Name}"),
-                new Claim("DateOfBirth", user.DateOfBirth.Value.ToString("yyyy-MM-dd")),
+                new Claim("DateOfBirth", user.DateOfBirth.ToString("yyyy-MM-dd")),
                 new Claim("Nationality", user.Nationality)
 
             };
 
             var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_authenticationSettings.JwtKey));
             var cred = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
-            var expires = DateTime.Now.AddDays(_authenticationSettings.JwtExpireDays);
+            var expires = DateTime.Now.AddDays(_authenticationSettings.ExpireDays);
 
             var token = new JwtSecurityToken(_authenticationSettings.JwtIssuer,
                 _authenticationSettings.JwtIssuer,
